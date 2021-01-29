383e6ed623c323b8b087703481855a1efd2f604b
Premakumar <premakumar.tr@gmail.com>
2021-01-29 19:29:48 +0530

    Update HelloWorld.java

--- src/main/java/com/examples/HelloWorld.java
+++ src/main/java/com/examples/HelloWorld.java
@@ -3,6 +3,6 @@ package com.examples;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("**************Hello World*************");
+		System.out.println("***************Hello World*************");
 	}
 }
